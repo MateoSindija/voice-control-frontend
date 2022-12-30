b450af4f571e07ecdd2e056323bc9aa5c1987627
Mateo SIndija <mtsindija05@gmail.com>
2022-12-30 19:57:05 +0100

    Added collision detection

--- scripts/audioCapture.js
+++ scripts/audioCapture.js
@@ -1,39 +1,61 @@
 let transcript = "";
+let isContinous = false;
+const checkboxContinous = document.querySelector("#continous");
+const microphoneSvg = document.querySelector("#microphone");
+const labelContionus = document.querySelector("#labelContionus");
+
+microphoneSvg.style.display = "none";
 
 if ("webkitSpeechRecognition" in window) {
-    // Initialize webkitSpeechRecognition
-    let speechRecognition = new webkitSpeechRecognition();
+  // Initialize webkitSpeechRecognition
+  let speechRecognition = new webkitSpeechRecognition();
 
-    speechRecognition.continuous = true;
-    speechRecognition.interimResults = true;
-    speechRecognition.lang = "en-AU";
+  speechRecognition.continuous = true;
+  speechRecognition.interimResults = true;
+  speechRecognition.lang = "en-AU";
 
-    speechRecognition.onresult = (event) => {
-        // Loop through the results from the speech recognition object.
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-            if (!event.results[i].isFinal) {
-                transcript = event.results[i][0].transcript;
-            }
-        }
+  speechRecognition.onresult = (event) => {
+    // Loop through the results from the speech recognition object.
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (!event.results[i].isFinal) {
+        transcript = event.results[i][0].transcript;
+      }
+      if (isContinous) {
+        characterMovementContinous(transcript);
+      } else {
+        characterMovementByStep(transcript);
+      }
 
-        characterMovement(transcript);
+      document.querySelector("#transcription").innerHTML = transcript;
+      transcript = "";
+    }
+  };
 
-        document.querySelector("#transcription").innerHTML = transcript;
-        transcript = "";
-    };
+  // Set the onClick property of the start button
+  document.querySelector("#start").onclick = () => {
+    checkboxContinous.disabled = true;
+    isContinous = checkboxContinous.checked;
+    // Start the Speech Recognition
+    speechRecognition.start();
 
-    // Set the onClick property of the start button
-    document.querySelector("#start").onclick = () => {
-        // Start the Speech Recognition
-        speechRecognition.start();
-    };
-    // Set the onClick property of the stop button
-    document.querySelector("#stop").onclick = () => {
-        // Stop the Speech Recognition
-        speechRecognition.stop();
-        character.x = 23;
-        character.y = 577;
-    };
+    microphoneSvg.style.display = "block";
+
+    document.querySelector("#start").disabled = true;
+    labelContionus.style.color = "grey";
+  };
+  // Set the onClick property of the stop button
+  document.querySelector("#stop").onclick = () => {
+    // Stop the Speech Recognition
+    speechRecognition.stop();
+    character.x = 23;
+    character.y = 577;
+
+    checkboxContinous.disabled = false;
+    document.querySelector("#start").disabled = false;
+    microphoneSvg.style.display = "none";
+    labelContionus.style.color = "black";
+    clearAllIntervals();
+  };
 } else {
-    console.log("Speech Recognition Not Available");
+  console.log("Speech Recognition Not Available");
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,65 +1,149 @@
+const CHAR_MOVE_MS = 500;
+const MOVE_T_L = 20;
+const MOVE_B_R = 10;
+
 let canvas = document.getElementById("game");
 canvas.width = 1500;
 canvas.height = 600;
 
-const ctx = canvas.getContext("2d");
+const circle = canvas.getContext("2d");
 const finishCtx = canvas.getContext("2d");
 
 const finish = {
-    x: 500,
-    y: 100,
-    size: 100,
+  x: 100,
+  y: 400,
+  size: 100,
 };
 
 const character = {
-    x: 23,
-    y: 577,
-    size: 10,
-    speed: 15,
+  x: 23,
+  y: 577,
+  size: 10,
+  speed: 15,
 };
 
 const borders = {
-    left: 0,
-    right: canvas.width - character.size,
-    top: 0,
-    bottom: canvas.height - character.size,
+  left: 0,
+  right: canvas.width - character.size,
+  top: 0,
+  bottom: canvas.height - character.size,
+};
+
+const isOnLeft = () => {
+  let distX = Math.abs(character.x - finish.x - finish.size / 2);
+  let distY = Math.abs(character.y - finish.y - finish.size / 2);
+
+  if (distX > finish.size / 2 + character.size / 2) {
+    return false;
+  }
+  if (distY > finish.size / 2 + character.size / 2) {
+    return false;
+  }
+
+  if (distX <= finish.size / 2) {
+    return true;
+  }
+  if (distY <= finish.size / 2) {
+    return true;
+  }
+
+  let dx = distX - finish.size / 2;
+  let dy = distY - finish.size / 2;
+  return dx * dx + dy * dy <= (character.size / 2) * (character.size / 2);
 };
 
 const update = () => {
-    requestAnimationFrame(update);
+  requestAnimationFrame(update);
 
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    finishCtx.clearRect(0, 0, canvas.width, canvas.height);
+  // Clear the canvas
+  circle.clearRect(0, 0, canvas.width, canvas.height);
+  finishCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-    finishCtx.fillStyle = "blue";
-    finishCtx.fillRect(finish.x, finish.y, finish.size, finish.size);
+  finishCtx.fillStyle = "blue";
+  finishCtx.fillRect(finish.x, finish.y, finish.size, finish.size);
 
-    // Draw the character
-    ctx.beginPath();
-    ctx.arc(character.x, character.y, 20, 0, 2 * Math.PI);
-    ctx.stroke();
+  // Draw the character
+  circle.beginPath();
+  circle.arc(character.x, character.y, 20, 0, 2 * Math.PI);
+  circle.fillStyle = "yellow";
+  circle.strokeStyle = "red";
+  circle.stroke();
+  circle.fill();
+
+  if (isOnLeft()) {
+    document.querySelector("#stop").click();
+    clearAllIntervals();
+    alert("You won");
+    return;
+  }
 };
 
-const characterMovement = (voiceCommand) => {
-    if (voiceCommand.includes("up")) {
-        character.y = Math.max(character.y - character.speed, borders.top + 20);
-    } else if (voiceCommand.includes("down")) {
-        character.y = Math.min(
-            character.y + character.speed,
-            borders.bottom - 10
-        );
-    } else if (voiceCommand.includes("left")) {
-        character.x = Math.max(
-            character.x - character.speed,
-            borders.left + 20
-        );
-    } else if (voiceCommand.includes("right")) {
-        character.x = Math.min(
-            character.x + character.speed,
-            borders.right - 10
-        );
-    }
+const clearAllIntervals = () => {
+  var killId = setTimeout(() => {
+    for (var i = killId; i > 0; i--) clearInterval(i);
+  }, 5);
+};
+
+const characterMovementByStep = (voiceCommand) => {
+  if (voiceCommand.includes("up")) {
+    character.y = Math.max(
+      character.y - character.speed,
+      borders.top + MOVE_T_L
+    );
+  } else if (voiceCommand.includes("down")) {
+    character.y = Math.min(
+      character.y + character.speed,
+      borders.bottom - MOVE_B_R
+    );
+  } else if (voiceCommand.includes("left")) {
+    character.x = Math.max(
+      character.x - character.speed,
+      borders.left + MOVE_T_L
+    );
+  } else if (voiceCommand.includes("right")) {
+    character.x = Math.min(
+      character.x + character.speed,
+      borders.right - MOVE_B_R
+    );
+  }
+};
+
+const characterMovementContinous = (voiceCommand) => {
+  if (voiceCommand.includes("up")) {
+    clearAllIntervals();
+    setInterval(() => {
+      character.y = Math.max(
+        character.y - character.speed,
+        borders.top + MOVE_T_L
+      );
+    }, [CHAR_MOVE_MS]);
+  } else if (voiceCommand.includes("down")) {
+    clearAllIntervals();
+    setInterval(() => {
+      character.y = Math.min(
+        character.y + character.speed,
+        borders.bottom - MOVE_B_R
+      );
+    }, [CHAR_MOVE_MS]);
+  } else if (voiceCommand.includes("left")) {
+    clearAllIntervals();
+    setInterval(() => {
+      character.x = Math.max(
+        character.x - character.speed,
+        borders.left + MOVE_T_L
+      );
+    }, [CHAR_MOVE_MS]);
+  } else if (voiceCommand.includes("right")) {
+    clearAllIntervals();
+    setInterval(() => {
+      character.x = Math.min(
+        character.x + character.speed,
+        borders.right - MOVE_B_R
+      );
+    }, [CHAR_MOVE_MS]);
+  } else if (voiceCommand.includes("stop")) {
+    clearAllIntervals();
+  }
 };
 
 update();
